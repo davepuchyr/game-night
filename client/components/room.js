164cdd6600db032d65c265a5722e7a931d00319c
Joshua Sohn <joshuadsohn@gmail.com>
2017-11-17 16:32:56 -0500

    adds whiteboard functions and edits room component

--- client/components/room.js
+++ client/components/room.js
@@ -2,6 +2,10 @@ import React, { Component }from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 
+
+import whiteboard, { draw } from '../whiteboard'
+
+
 class Room extends Component {
     constructor(props) {
         super(props)
