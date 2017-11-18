9d14d2aee48fcfcb471d133396f44838efd79572
Joshua Sohn <joshuadsohn@gmail.com>
2017-11-18 16:12:08 -0500

    fixes logic flow of socket connections

--- server/socket/index.js
+++ server/socket/index.js
@@ -19,6 +19,8 @@ module.exports = (io) => {
         return Object.values(user)[0]
       })
       socket.emit('updateOnlineUsers', userIdArr)
+    } else {
+      socket.emit('updateOnlineUsers', currentUsersOnlineArr)      
     }
     console.log('ONLINE USERS ARE ', onlineUsers)
   })
