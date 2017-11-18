c52377dce4b6aa33ea82a13d53bb10becde059d6
yeb9925 <seungt.han@gmail.com>
2017-11-17 19:21:09 -0500

    merged staging branch with login branch

---
+++ client/components/room.js
@@ -0,0 +1,20 @@
+import React, { Component }from 'react'
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
+
+
+import whiteboard, { draw } from '../whiteboard'
+
+
+class Room extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render () {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./user')
+const Room = require('./room')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,6 +8,9 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+Room.belongsTo(User, { as: 'admin' })
+Room.belongsToMany(User, { as: 'players', through: 'room_players' })
+
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -14,5 +18,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Room
 }
