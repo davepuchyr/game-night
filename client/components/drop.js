eaa7b5ccdb5840b2b45578273c468412c0f58993
Joshua Sohn <joshuadsohn@gmail.com>
2017-11-22 11:46:34 -0500

    dropping images works in drop component

---
+++ client/components/drop.js
@@ -0,0 +1,71 @@
+import React, { Component }from 'react'
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
+import Dropzone from 'react-dropzone'
+import request from 'superagent';
+
+const CLOUDINARY_UPLOAD_PRESET = 'gamenight';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/gamenight/upload';
+
+class Drop extends Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            uploadedFileCloudinaryUrl: '',
+            uploadedFile: ''
+          }
+        this.onImageDrop = this.onImageDrop.bind(this)
+        this.handleImageUpload = this.handleImageUpload.bind(this)
+    }
+
+    onImageDrop(files) {
+        this.setState({
+          uploadedFile: files[0]
+        })
+        this.handleImageUpload(files[0]);
+    }
+    
+    handleImageUpload(file) {
+        let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                            .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                            .field('file', file);
+    
+        upload.end((err, response) => {
+          if (err) {
+            console.error(err);
+          }
+    
+          if (response.body.secure_url !== '') {
+            this.setState({
+              uploadedFileCloudinaryUrl: response.body.secure_url
+            });
+          }
+        });
+      }
+
+    render () {
+        console.log('drop rendered')
+        return (
+            <div>
+                    <h1> DROP </h1>
+                <div className="FileUpload">
+                    <Dropzone
+                        multiple={false}
+                        accept="image/*"
+                        onDrop={this.onImageDrop}>
+                        <p>Drop an image or click to select a file to upload.</p>
+                    </Dropzone>
+                </div>
+                    <div>
+                    {this.state.uploadedFileCloudinaryUrl === '' ? null :
+                    <div>
+                        <p>{this.state.uploadedFile.name}</p>
+                        <img src={this.state.uploadedFileCloudinaryUrl} />
+                    </div>}
+                </div>
+          </div>
+        )
+    }
+}
+
+export default Drop
