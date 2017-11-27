512fcf6497099d589e3187ee172874dc11266f53
Joshua Sohn <joshuadsohn@gmail.com>
2017-11-27 16:50:31 -0500

    option for draw logic in mainstage

--- client/components/canvaselements/MainStage.js
+++ client/components/canvaselements/MainStage.js
@@ -18,7 +18,9 @@ class MainStage extends React.Component {
       dragStart: [],
       dragOffSet: [0, 0],
       canvas: null,
-      context: null
+      context: null,
+      isDrawing: false,
+      mode: "brush",
     }
 
     this.moveStageOnHover = this.moveStageOnHover.bind(this)
@@ -39,10 +41,85 @@ class MainStage extends React.Component {
     }
     document.addEventListener("keydown", this.handleKeyDown);
     document.addEventListener("keyup", this.handleKeyUp);
+    // let stage = this.refs.mainstage
     let canvas = this.refs.mainstage._stage.children[0].canvas._canvas
-    let context = canvas.getContext("2d")
-    this.setState({ canvas, context }, () => console.log("!!!!!!!!!", this.state.canvas))
+    // let context = canvas.getContext("2d")
+    // let canvas = document.createElement("canvas");
+    // stage.add(canvas)
+    // canvas.width = 300;
+    // canvas.height = 300;
+    let context = canvas.getContext("2d");
+
+    this.setState({ canvas, context })
   }
+
+
+  handleMouseDown = (e) => {
+    console.log("mousedown!!!!!!!!!!!!!!!", e.target);
+    if (this.state.shift){
+        this.setState({ isDrawing: true });
+    }
+
+    // TODO: improve
+    let stage = this.refs.mainstage._stage
+    // const stage = this.refs.mainstage
+    console.log("FROM e.target ", e.target)
+    this.lastPointerPosition = stage.getPointerPosition();
+    console.log("FROM lastPointerPosition ", this.lastPointerPosition)
+    // this.lastPointerPosition.x = this.lastPointerPosition.x - this.props.offSet[0]
+    // this.lastPointerPosition.y = this.lastPointerPosition.y - this.props.offSet[1]
+  };
+
+  handleMouseUp = () => {
+    console.log("mouseup");
+    this.setState({ isDrawing: false });
+  };
+
+  handleMouseMove = () => {
+    // console.log('mousemove!!!!!!!', this);
+    const { context, isDrawing, mode } = this.state;
+
+    if (isDrawing) {
+      // console.log("DRAW")
+      // TODO: Don't always get a new context
+      context.strokeStyle = "#df4b26";
+      context.lineJoin = "round";
+      context.lineWidth = 5;
+
+      if (mode === "brush") {
+        context.globalCompositeOperation = "source-over";
+      } else if (mode === "eraser") {
+        context.globalCompositeOperation = "destination-out";
+      }
+      context.beginPath();
+
+      var localPos = {
+        x: (this.lastPointerPosition.x),
+        y: (this.lastPointerPosition.y)
+      };
+    //   console.log("moveTo", localPos);
+      context.moveTo(localPos.x, localPos.y);
+
+      // TODO: improve
+      // const stage = this.image.parent.parent;
+      let stage = this.refs.mainstage._stage
+
+      var pos = stage.getPointerPosition();
+      localPos = {
+        x: (pos.x),
+        y: (pos.y)
+      };
+    //   console.log("lineTo", localPos);
+      context.lineTo(localPos.x, localPos.y);
+      context.closePath();
+      context.stroke();
+      this.lastPointerPosition = pos;
+      // console.log()
+      this.refs.image.draw();
+      console.log('!!!!!!!!!!!!!', this.refs.image.getLayer())
+      // this.refs.mainstage._stage.children[0].canvas.getLayer().draw();
+    }
+  };
 
   handleKeyDown (e) {
       if (e.key === 'Shift') this.setState({shift: true})
@@ -74,12 +151,14 @@ class MainStage extends React.Component {
 
   render() {
     console.log(this.state)
+    const { canvas } = this.state;
     const { black, red, green, blue } = this.props.tokens
     const { images } = this.props
     const { rId } = this.props    
 
     return (
-      <Stage 
+      <Stage
+        image={canvas}
         name="mainstage"
         ref="mainstage"
         width={window.innerWidth} 
@@ -94,17 +173,21 @@ class MainStage extends React.Component {
           height={window.innerHeight}
           >
           <Image 
+            ref="image"
             className="dragImg" name="background" 
             image={this.state.backgroundImage} 
             onMouseOver={this.moveStageOnHover}
             onMouseOut={this.handleMouseOut}
+            onMouseDown={this.handleMouseDown}
+            onMouseUp={this.handleMouseUp}
+            onMouseMove={this.handleMouseMove}
             />
-          <Drawing
+          {/* <Drawing
             shift={this.state.shift}
             offSet={this.state.dragOffSet}
             canvas={this.state.canvas}
             context={this.state.context}
-          />
+          /> */}
           <HexPiece id={rId} fill={'black'} x={black[0]} y={black[1]}/>
           <HexPiece id={rId} fill={'red'} x={red[0]} y={red[1]}/>
           <HexPiece id={rId} fill={'green'} x={green[0]} y={green[1]}/>
@@ -138,6 +221,16 @@ class MainStage extends React.Component {
             />
           })
           }
+            {/* <Image
+              image={canvas}
+              ref={node => (this.image = node)}
+              width={window.innerWidth}
+              height={window.innerHeight}
+              stroke={'blue'}
+              onMouseDown={this.handleMouseDown}
+              onMouseUp={this.handleMouseUp}
+              onMouseMove={this.handleMouseMove}
+          /> */}
         </Layer>
       </Stage>
     );
