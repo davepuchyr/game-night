8ad020d21c1d9fa65b8fb3fd4128228fce158393
hyunjkim <hkim6501@gmail.com>
2017-11-28 10:28:40 -0500

    passport will not allow me to access to the api/rooms SERVER side

--- server/api/rooms.js
+++ server/api/rooms.js
@@ -3,20 +3,20 @@ const {Room, User} = require('../db/models')
 const {passport} = require('../index.js')
 module.exports = router
 
-// router.get('/', (req,res,next) => {
-//   passport.authenticate('local',(err, user, info) => {
-//     console.log('LINE 7' , err)
-//     console.log('LINE 8', user)
-//     console.log('LINE 9', info)
-//       Room.findAll({
-//         // explicitly select only the id and email fields - even though
-//         // users' passwords are encrypted, it won't help if we just
-//         // send everything to anyone who asks!
-//         })
-//         .then(allRooms => res.json(allRooms))
-//         .catch(next)
-//   })
-// })
+router.get('/', (req,res,next) => {
+  passport.authenticate('local',(err, user, info) => {
+    console.log('LINE 7' , err)
+    console.log('LINE 8', user)
+    console.log('LINE 9', info)
+      Room.findAll({
+        // explicitly select only the id and email fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        })
+        .then(allRooms => res.json(allRooms))
+        .catch(next)
+  })
+})
 
 //type , name
 router.post('/',  passport.authenticate('local',{failureRedirect: '/login'}),(req, res, next) => {
@@ -38,9 +38,19 @@ router.use((req, res, next) => {
 
 
 
-router.get('/', (req, res, next) => {
-  passport.authenticate('local',{successRedirect: '/api/rooms',failureRedirect: '/login'})(req, res, next)
-      Room.findAll()
-      .then(allRooms => res.json(allRooms))
-      .catch(next)
-});
+// router.get('/', (req, res, next) => {
+//   passport.authenticate('local',{successRedirect: '/api/rooms',failureRedirect: '/login'})(req, res, next)
+//       Room.findAll()
+//       .then(allRooms => res.json(allRooms))
+//       .catch(next)
+// });
+
+
+// router.get('/',
+//   // passport.authenticate('local',{ session: false }),
+//   (req, res,next) => {
+//     console.log('HITS HERE?')
+//       Room.findAll()
+//       .then(allRooms => res.json(allRooms))
+//       .catch(next)
+//   });
