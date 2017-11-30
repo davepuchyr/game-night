a5310bd5572f3d6f13a66b44a7028d77a1a6d104
hyunjkim <hkim6501@gmail.com>
2017-11-30 02:16:57 -0500

    generate youtube videos in the background

--- client/components/SplashScreen.js
+++ client/components/SplashScreen.js
@@ -1,55 +1,41 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { Login, Signup } from './index'
+
 /**
  * COMPONENT
  * All credits to : https://codepen.io/SimonEvans/pen/weoLLB
  */
 
- // const randomVideos = () => {
- //   let videos = ["Stars.mp4","Balls_falling.mp4","Toy_train.mp4","Dices.mp4","Domino.mp4","http://mazwai.com/system/posts/videos/000/000/148/preview_mp4/kenji_kawasawa-106_balloons.mp4?1419192915"]
- //   return videos[Math.floor(Math.random()*(videos.length-0))+0]
- // }
-
  const SplashScreen = (props) => {
-  const {isLoggedIn} = props
-  return (
-          <div>
-            <div className="container-main splash">
-               <video
-                  autoplay=""
-                  playsinline=""
-                  preload=""
-                  loop="true"
-                  muted="true"
-                  poster="http://i.imgur.com/xHO6DbC.png">
-                 {/*<source src={randomVideos()}/>*/}
-                 <source src="http://mazwai.com/system/posts/videos/000/000/148/preview_mp4/kenji_kawasawa-106_balloons.mp4?1419192915"/>
-               </video>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 500 80"
-                  preserveAspectRatio="xMidYMid slice">
-                 <defs>
-                   <mask id="mask" x="0" y="0" width="100%" height="100%" >
-                     <rect x="0" y="0" width="100%" height="100%" />
-                     <text x="184.5" y="35">Game</text>
-                     <text x="183" y="75">Night</text>
-                   </mask>
-                 </defs>
-                 <rect x="0" y="0" width="100%" height="100%" />
-               </svg>
-            </div>
-            <div className="container-main-button">
-             <button>
-              <Link to="/login">Login</Link>
-              </button>
-             <button>
-              <Link to="/signup">Signup</Link>
-            </button>
-            </div>
-          </div>
-         )
+	const {isLoggedIn} = props
+	return (
+					<div className="container-main splashscreen">
+						<div className="container-main splashscreen-video">
+							<iframe width="1280" height="720" src="https://www.youtube.com/embed/LJ0zGxxO_Wc?list=PL9X62Wu-yBvUAUk93hLb3N447RRnNW05G&rel=0&version=3&autoplay=1&loop=1&controls=0&showinfo=0&mute=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen>
+							</iframe>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 80" preserveAspectRatio="xMidYMid slice">
+								<defs>
+									<mask id="mask" x="0" y="0" width="100%" height="100%" >
+										<rect x="0" y="0" width="100%" height="100%" />
+										<text x="195" y="30">Game</text>
+										<text x="195" y="60">Night</text>
+
+									 </mask>
+								</defs>
+								<rect x="0" y="0" width="100%" height="100%" />
+							</svg>
+						</div>
+						<div className="container-main splashscreen-login-button">
+							<button id="loginBtn">Login
+								<Link to="/login"></Link>
+							</button>
+							<button id="signupBtn">Signup
+								<Link to="/signup"></Link>
+							</button>
+						</div>
+					</div>
+				 )
 }
 
 export default SplashScreen
