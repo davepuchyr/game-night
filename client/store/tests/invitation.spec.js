16c87e82f4258e18c183fc270aff706fcebb3bda
Matthew Smith <mattsheridansmith@gmail.com>
2017-12-07 17:20:12 -0500

    Readded store user tests.

---
+++ client/store/tests/invitation.spec.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai'
+import { createStore } from 'redux'
+import { getInvite, getInvitations } from '../invitation'
+import { default as invitationReducer } from '../background'
+
+
+
