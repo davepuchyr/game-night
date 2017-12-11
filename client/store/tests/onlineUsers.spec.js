3833008c7f4a70a4bc9e005eebaee9e8ca91b66a
Matthew Smith <mattsheridansmith@gmail.com>
2017-12-11 17:16:09 -0500

    Updated onlineUsers store and testing.

---
+++ client/store/tests/onlineUsers.spec.js
@@ -0,0 +1,37 @@
+import { expect } from 'chai'
+import { createStore } from 'redux'
+import { getOnlineUsers } from '../onlineUsers'
+import { default as onlineUsersReducer } from '../onlineUsers'
+
+const mockUsersOnline = [{id: 4, nickname: 'MattyAmazing'},
+{id: 3, nickname: 'retrofuturejosh'}]
+const initialState = []
+
+describe('Store: Online Users', () => {
+  describe('Actions:', () => {
+
+    it('getOnline users returns a correct action', () => {
+      expect(getOnlineUsers(mockUsersOnline)).to.be.deep.equal({type: 'GET_ONLINE_USERS', onlineUsers: mockUsersOnline})
+    })
+
+  })
+
+  describe('Reducer:', () => {
+    describe('Reducer', () => {
+      let testStore
+  
+      beforeEach('make mock store', () => {
+        testStore = createStore(onlineUsersReducer)
+      })
+  
+      it('has proper initial state', () => {
+        expect(testStore.getState()).to.be.deep.equal(initialState)
+      })
+  
+      it('properly adds an image to state', () => {
+        testStore.dispatch({type: 'GET_ONLINE_USERS', onlineUsers: mockUsersOnline})
+        expect(testStore.getState()).to.be.deep.equal(mockUsersOnline)
+      })
+    })
+  })
+})
