182d2b4a33288c2a1354123f220b5bf34282e23f
yeb9925 <seungt.han@gmail.com>
2018-02-21 14:09:25 -0500

    front end test specs done

--- client/components/tests/dropGroup.spec.js
+++ client/components/tests/dropGroup.spec.js
@@ -1,54 +1,54 @@
-import React from 'react'
+import React from 'react';
 
-import chai, { expect } from 'chai'
-import { shallow, mount, render } from 'enzyme'
+import chai, { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
 import sinon from 'sinon';
 
-import socket from '../../socket'
-import { DropGroup } from '../DropGroup'
+import socket from '../../socket';
+import { DropGroup } from '../DropGroup';
 
 describe('Component: DropGroup', () => {
-    let dropWrapper
+  let dropWrapper;
 
-    beforeEach('create DropGroup component wrapper', () => {
-      dropWrapper = shallow(<DropGroup/>)
-    })
+  beforeEach('create DropGroup component wrapper', () => {
+    dropWrapper = shallow(<DropGroup/>);
+  });
 
-    describe('state check', () => {
-      it('has proper initial state', () => {
-        expect(dropWrapper.state()).to.deep.equal({
-          hover: false
-        })
-      })
-    })
+  describe('state check', () => {
+    it('has proper initial state', () => {
+      expect(dropWrapper.state()).to.deep.equal({
+        hover: false
+      });
+    });
+  });
 
-    describe('view check', () => {
-      it('should render Dropzone component', () => {
-        expect(dropWrapper.find('Dropzone')).to.have.length(1)
-      })
+  describe('view check', () => {
+    it('should render Dropzone component', () => {
+      expect(dropWrapper.find('Dropzone')).to.have.length(1);
+    });
 
-      it('should render trash icon', () => {
-        expect(dropWrapper.find('img')).to.have.length(1)
-      })
-    })
+    it('should render trash icon', () => {
+      expect(dropWrapper.find('img')).to.have.length(1);
+    });
+  });
 
-    describe('actions and functions', () => {
-      it('handleMouseOver should correctly set state.hover', () => {
-        DropGroup.prototype.handleMouseOver.call(dropWrapper)
-        expect(dropWrapper.state().hover).to.equal(true)
-      })
+  describe('actions and functions', () => {
+    it('handleMouseOver should correctly set state.hover', () => {
+      DropGroup.prototype.handleMouseOver.call(dropWrapper);
+      expect(dropWrapper.state().hover).to.equal(true);
+    });
 
-      it('handleMouseOver should change drop icon src', () => {
-        expect(dropWrapper.find('img').props().src).to.equal('/assets/broadcast_image_icon.png')
-        DropGroup.prototype.handleMouseOver.call(dropWrapper)
-        expect(dropWrapper.find('img').props().src).to.equal('/assets/broadcast_image_icon_mix2.png')
-      })
-      
-      it('handleMouseLeave should correctly set state.hover', () => {
-        DropGroup.prototype.handleMouseOver.call(dropWrapper)
-        expect(dropWrapper.state().hover).to.equal(true)
-        DropGroup.prototype.handleMouseLeave.call(dropWrapper)
-        expect(dropWrapper.state().hover).to.equal(false)
-      })
-    })
-})
+    it('handleMouseOver should change drop icon src', () => {
+      expect(dropWrapper.find('img').props().src).to.equal('/assets/broadcast_image_icon.png');
+      DropGroup.prototype.handleMouseOver.call(dropWrapper);
+      expect(dropWrapper.find('img').props().src).to.equal('/assets/broadcast_image_icon_mix2.png');
+    });
+    
+    it('handleMouseLeave should correctly set state.hover', () => {
+      DropGroup.prototype.handleMouseOver.call(dropWrapper);
+      expect(dropWrapper.state().hover).to.equal(true);
+      DropGroup.prototype.handleMouseLeave.call(dropWrapper);
+      expect(dropWrapper.state().hover).to.equal(false);
+    });
+  });
+});
--- client/components/tests/onlineUsers.spec.js
+++ client/components/tests/onlineUsers.spec.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import chai, { expect } from 'chai'
-import { shallow, mount, render } from 'enzyme'
+import React from 'react';
+import chai, { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
 import sinon from 'sinon';
 
-import { OnlineUsers } from '../onlineUsers'
+import { OnlineUsers } from '../onlineUsers';
 
 describe('Component: OnlineUsers', () => {
-    let onlineUsersWrapper
-    const users = [
-        {nickname: 'Josh'},
-        {nickname: 'Matt'}
-    ]
+  let onlineUsersWrapper;
+  const users = [
+    {nickname: 'Josh'},
+    {nickname: 'Matt'}
+  ];
 
-    beforeEach('create OnlineUsers component wrapper', () => {
-      onlineUsersWrapper = shallow(<OnlineUsers
-        users={users}/>)
-    })
+  beforeEach('create OnlineUsers component wrapper', () => {
+    onlineUsersWrapper = shallow(<OnlineUsers
+      users={users}/>);
+  });
 
-    describe('view check', () => {
-      it('renders a list of online Users', () => {
-        expect(onlineUsersWrapper.find('.container-main-lobby-bottom-comps-players-onlineUsers-users')).to.have.length(2)
-      })
-    })
+  describe('view check', () => {
+    it('renders a list of online Users', () => {
+      expect(onlineUsersWrapper.find('.container-main-lobby-bottom-comps-players-onlineUsers-users')).to.have.length(2);
+    });
+  });
 
-    it('displays nicknames of users', () => {
-      expect(onlineUsersWrapper.find('.container-main-lobby-bottom-comps-players-onlineUsers-users').first().text()).to.equal('Josh')
-    })
-})
+  it('displays nicknames of users', () => {
+    expect(onlineUsersWrapper.find('.container-main-lobby-bottom-comps-players-onlineUsers-users').first().text()).to.equal('Josh');
+  });
+});
